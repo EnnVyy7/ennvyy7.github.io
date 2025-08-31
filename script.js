3c15c388958f56622a91d5c599ebc3fd5f75320f
EnnVyy7 <nvaldinata1@gmail.com>
2025-08-31 11:29:22 +0700

    kelanjutan backgriund dan navbar

---
+++ script.js
@@ -0,0 +1,5 @@
+// window.addEventListener('scroll', () => {
+//   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+//     document.body.classList.add('dark');
+//   }
+// });
